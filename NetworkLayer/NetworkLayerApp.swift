5e67740bad0abf63afc2e31ca9691b60b1c84f05
Han Sena <abisena@outlook.com>
2024-06-28 13:33:54 +0700

    Initial Commit

---
+++ NetworkLayer/NetworkLayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkLayerApp.swift
+//  NetworkLayer
+//
+//  Created by Hidayat Abisena on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkLayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
